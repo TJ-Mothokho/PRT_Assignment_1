9c1ed42ca3d38def809d6b084ac6fca2795a3f55
Tshiamo Justice Mothokho <s225258358@mandela.ac.za>
2023-06-10 02:09:26 +0200

    last

--- PRT_Assignment_1/Form1.cs
+++ PRT_Assignment_1/Form1.cs
@@ -12,6 +12,8 @@ namespace PRT_Assignment_1
 {
     public partial class Form1 : Form
     {
+        Methods method = new Methods();
+        int lowerNum = 1, MaxNum = 102;
         public Form1()
         {
             InitializeComponent();
@@ -23,9 +25,15 @@ namespace PRT_Assignment_1
             this.WindowState = FormWindowState.Maximized;
         }
 
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            int NumbertoFind = GetTheNumber("What is the Number you are Looking for? ", lowerNum, MaxNum);
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            //vs
         }
+
     }
 }
---
+++ PRT_Assignment_1/Methods.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRT_Assignment_1
+{
+    internal class Methods
+    {
+        public int[] Read()
+        {
+            StreamReader reader = new StreamReader("AssignementData.txt");
+            string Num = " ";
+            int i = 0;
+            int[] arrayNumber = new int[500];
+
+
+            while ((Num = reader.ReadLine()) != null)
+            {
+                arrayNumber[i] = Convert.ToInt32(Num);
+
+                i++;
+
+            }
+            reader.Close();
+            return arrayNumber;
+        }
+
+        static int GetTheNumber(string prompt, int lower, int maxNum)
+        {
+
+            int Num = 0;
+            bool valid;
+
+            do
+            {
+                try
+                {
+                    valid = true;
+                    Console.Write(prompt);
+                    Num = int.Parse(Console.ReadLine());
+
+                    if (Num < lower || Num > maxNum)
+                    {
+                        Console.WriteLine("Invalid number entered. Number should be between {0} and {1}", lower, maxNum);
+                        valid = false;
+                    }
+                }
+                catch (System.FormatException ex)
+                {
+                    Console.WriteLine("Wrong number entered. Please enter numerical data");
+                    valid = false;
+                }
+
+            } while (valid == false);
+            return Num;
+
+        }
+    }
+}
